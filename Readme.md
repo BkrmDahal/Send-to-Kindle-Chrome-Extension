58151127cc4038df442daffc15c73d941e47a1ec
bkrm dahal <bkrm.dahal@gmail.com>
2021-10-14 00:53:41 +0545

    added one working version

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 # Send-to-Kindle-Chrome-Extension [WIP]
 
 Chrome extension to send any article to your kindle.
+
+# TODO
+- [] make popup more interactive
+- [] ask user for api and email at begaining only
--- popup.js
+++ popup.js
@@ -1,23 +1,38 @@
-$(document).ready(function() {
-   $("button").click(function() {
-       var xhr = new XMLHttpRequest();
-       var url = "https://webhook.site/0e185470-c8d4-4de0-b384-71746486e3ab";
-       var email = $('#email').val();
-       var currentURL = window.location.href
-       console.log(currentURL)
-       xhr.open("POST", url, true);
-       xhr.setRequestHeader("Content-Type", "application/json");
-       xhr.onreadystatechange = function() {
-           if (xhr.readyState === 4 && xhr.status === 200) {
-               var json = JSON.parse(xhr.responseText);
-               console.log(json)
+function sendData(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+        var tabURL = tabs[0].url;
+        var email = document.getElementById('email').value
+        var SERVER_URL = 'https://xxx.ws-us17.gitpod.io/api/v1/send/kindle/'
+        var data = JSON.stringify({
+            "url": tabURL,
+            "email": email
+        });
+        (async () => {
+            const rawResponse = await fetch(SERVER_URL, {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: data
+            });
+            const content = await rawResponse.text();
+            // document.getElementById('status').value=content;
+            console.log(content);
+          })();
 
-           }
-       };
-       var data = JSON.stringify({
-           "email": email, 
-           "url": currentURL
-       });
-       xhr.send(data);
-   });
-});
+        // var xhr = new XMLHttpRequest();
+        // xhr.onreadystatechange = function() {
+        //     var json = JSON.parse(xhr.responseText);
+        //     document.getElementById('button').value = 'SUCCESS'
+        // };
+
+        // xhr.open("POST", 'https://webhook.site/0e185470-c8d4-4de0-b384-71746486e3ab');
+        // xhr.send(data);
+    }
+    );
+   };
+
+var button = document.getElementById('button')
+if (button){
+addEventListener('click', sendData, false)}
