0c803b7ad0859d1d0fd6053562db44fd6addd6bf
bkrm dahal <bkrm.dahal@gmail.com>
2021-10-14 12:44:44 +0545

    working version with interactive button

--- popup.js
+++ popup.js
@@ -1,31 +1,57 @@
-function sendData(){
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-        var tabURL = tabs[0].url;
-        var email = document.getElementById('email').value
-        var SERVER_URL = 'https://xxx.ws-us17.gitpod.io/api/v1/send/kindle/'
+function scrapeThePage() {
+  // Keep this function isolated - it can only call methods you set up in content scripts
+  var htmlCode = document.documentElement.outerHTML;
+  return htmlCode;
+}
 
-        var data = JSON.stringify({
-            "url": tabURL,
-            "email": email,
+function sendData() {
+  chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  }, function (tabs) {
+    // show process in button
+    document.getElementById('button').innerHTML = "processing..."
+
+    //get url and other detail
+    var tabURL = tabs[0].url;
+    var email = document.getElementById('email').value
+    var SERVER_URL = 'https://webhook.site/0e185470-c8d4-4de0-b384-71746486e3ab' // need to chnage this to backend api
+
+    // We have to convert the function to a string
+    const scriptToExec = `(${scrapeThePage})()`;
+
+    // Run the script in the context of the tab
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: scriptToExec
+    }, function (results) {
+
+      var html = results[0]
+      var data = JSON.stringify({
+        "url": tabURL,
+        "email": email,
+        "html": html
+      });
+
+      (async () => {
+        const rawResponse = await fetch(SERVER_URL, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: data
         });
+        const content = await rawResponse.text();
+        const status = await rawResponse.ok;
+        console.log(status)
+        if (status) {
+          document.getElementById('button').innerHTML = "success"
+        } else {
+          document.getElementById('button').innerHTML = "failed"
+        }
+      })();
+    });
+  });
+};
 
-        (async () => {
-            const rawResponse = await fetch(SERVER_URL, {
-              method: 'POST',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: data
-            });
-            const content = await rawResponse.text();
-            // document.getElementById('status').value=content;
-            console.log(content);
-          })();
-    }
-    );
-   };
-
-var button = document.getElementById('button')
-if (button){
-addEventListener('click', sendData, false)}
+var button = document.getElementById('button').addEventListener('click', sendData)
